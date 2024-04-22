0fcc659ff67fc7aa9d43116f4a42d7a1b2253e13
stnford <122490108+stnford@users.noreply.github.com>
2024-04-22 11:16:48 -0500

    Update main.cpp
    added charecter input in main

--- main.cpp
+++ main.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 // updates from user1
 int main(){
-
-
+  
+  string item1, item2;
+  
+  cin.ignore();
+  cout << "  Enter two items: " ;
+  getline(cin, item1, " ");
+  getline(cin, item2);
+  
   return 0;
 }
